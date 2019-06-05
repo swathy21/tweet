736232993d396f2dd0f66f96d7c9614c12b0c779
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-06-04 22:49:19 -0700

    Create pgm20.py

---
+++ pgm20.py
@@ -0,0 +1,10 @@
+num=int(input("enter the number"))
+sum=0
+temp=num
+while temp>0:
+digit=num%10
+sum+=digit**3
+if(num==sum)
+print("the number is amstrong")
+else
+print("number is not an amstrong number")
